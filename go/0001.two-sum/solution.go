d421429de197226833f76b831b3ddbea7a42f201
ctgardner <10970827+ctgardner@users.noreply.github.com>
2026-05-05 12:26:24 +1000

    Add go/0001.two-sum

---
+++ go/0001.two-sum/solution.go
@@ -0,0 +1,38 @@
+// Created by Callum Gardner at 2026/03/11 13:05
+// leetgo: dev
+// https://leetcode.com/problems/two-sum/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	. "github.com/j178/leetgo/testutils/go"
+)
+
+// @lc code=begin
+
+func twoSum(nums []int, target int) (ans []int) {
+	for i := 0; i < len(nums); i++ {
+		for j := i+1; j < len(nums); j++ {
+			if nums[i] + nums[j] == target {
+				ans = []int{i, j}
+				break
+			}
+		}
+	}
+	return
+}
+
+// @lc code=end
+
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+	nums := Deserialize[[]int](ReadLine(stdin))
+	target := Deserialize[int](ReadLine(stdin))
+	ans := twoSum(nums, target)
+
+	fmt.Println("\noutput:", Serialize(ans))
+}
